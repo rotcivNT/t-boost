762551af27373b90eb5eab18ac43e9a6e99c280a
rotciv <ngocthang.devweb@gmail.com>
2024-08-12 12:31:17 +0700

    a

--- app/workspace/[workspaceId]/message/home/[cid]/page.tsx
+++
@@ -1,8 +0,0 @@
-import DirectMessagesContent from "@/components/conversation/direct-messages/direct-messages-content/DirectMessagesContent";
-
-async function DCMPage({ params }: { params: { [key: string]: string } }) {
-  const cid = params.cid as string;
-  return <DirectMessagesContent cid={cid.slice(1)} />;
-}
-
-export default DCMPage;
--- app/workspace/[workspaceId]/message/page.tsx
+++
@@ -1,5 +0,0 @@
-function MessagePage() {
-  return <div>Message</div>;
-}
-
-export default MessagePage;
